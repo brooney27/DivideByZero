726089bc3a534d75afcf7b162f6202f47d375603
brooney27 <brooney278@gmail.com>
2016-06-16 15:32:39 -0400

    ayyy

--- src/DivideByZero.java
+++ src/DivideByZero.java
@@ -2,7 +2,7 @@
 public class DivideByZero {
 	public static void main(String[] args){
 		int a = 1;
-		int b = 0;
+		int b = 1;
 		
 		try{
 			
@@ -12,6 +12,8 @@ public class DivideByZero {
 			System.out.println("Can't divide by zero!");
 		}catch(Exception e){
 			System.out.println("Found a problem while dividing");
+		}finally {
+		    System.out.println("finally block will execute.");
 		}
 	}
 }
